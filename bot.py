68ac4d2e7a6a6da65e44af95c47453bfc9337144
fkokeralam123 <82272962+fkokeralam123@users.noreply.github.com>
2021-06-07 19:10:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,6 @@ class Bot(Client):
         self.USER, self.USER_ID = await User().start()
         await self.USER.send_message(
             chat_id=usr_bot_me.username,
-            text="😬🤒🤒"
         )
 
     async def stop(self, *args):
