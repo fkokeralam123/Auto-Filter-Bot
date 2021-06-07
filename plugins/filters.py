98c64f45ec7c665673cb66f04be30d3cb6b4e7bb
fkokeralam123 <82272962+fkokeralam123@users.noreply.github.com>
2021-06-07 19:16:47 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -158,8 +158,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.answer()
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                    InlineKeyboardButton("GROUP", url="https://t.me/Movie_Collections_Group")],
+                [InlineKeyboardButton("⭕️JOIN OUR CHANNEL ⭕️", url="https://t.me/Movie_Collections_channel")]
             ])
 
             await query.message.edit_text(
@@ -189,7 +189,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/fkokeralam123/Auto-Filter-Bot")]
+                [InlineKeyboardButton("GOOGLE", url="https://google.com")]
             ])
 
             await query.message.edit_text(
